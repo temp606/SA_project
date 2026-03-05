89c1d80089ebef9bbc7587556ca952a9b3955b21
manideep8008 <manideepreddyboddu@gmail.com>
2026-03-05 12:32:32 -0600

    some Update

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-temp = 42
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True)
